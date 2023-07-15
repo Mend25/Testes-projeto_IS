bcda2b47d30397cc3f15c957f9cc538ad0f84fb6
Mend25 <113634496+Mend25@users.noreply.github.com>
2023-07-15 11:31:21 -0300

    Update menu.asm

--- menu.asm
+++ menu.asm
@@ -1,45 +1,87 @@
 org 0x7c00
-jmp _start
+jmp menu
 
-message1 db "Press enter to start", 10, 13
-message2 db "Instructions", 0
+message1 db "             SOCCER PONG", 10, 13, 10, 13, 10, 13, 10, 13, "        Press ENTER to start", 10, 13, "        Press SPACE to instructions",0
+message2 db "This is one of the hardest pong games in world history!!!", 10, 13,"Try to survive the longest!!!", 10, 13, 10, 13, 10, 13, 10, 13, "        How to play:", 10, 13, 10, 13, 10, 13, "        W -> up", 10, 13, 10, 13, "        S -> down", 10, 13, 10, 13, 10, 13, 10, 13, 10, 13, "Press SPACE to menu",0
 
-_start:
-    xor ax, ax
-    xor si, si
-    xor bx, bx
-    
-    mov ah, 0
-    mov al, 12h
+menu:
+    mov ah, 0 
+    mov al, 13h
     int 10h
-    
-    mov bl, 0xf
-    
+
+    mov bl, 0xf 
     mov ah, 0eh
     mov bh, 0
     mov bl, 2
-    
-    mov ah, 0x0e
+
     mov si, message1
     call print_loop
-    
-    xor si, si
-    mov si, message2
-    call print_loop
-    
+    xor ax, ax
+    call waitStart
 
-    call done
-    
+
+    xor ax, ax
+
+    mov ah, 0 
+    mov al, 12h
+    int 10h
+
 
 print_loop:
     lodsb
     cmp al, 0
     je .done
-    int 0x10
+    call putchar
     jmp print_loop
-    
+
     .done:
-        ret    
+        ret
+
+
+waitStart:
+    call getchar_m
+
+    cmp al, 0x0d
+    je .done
+
+    cmp al, " "
+    je instructions
+
+    jmp waitStart
+
+    .done:
+        cmp 
+        ret
+
+instructions:
+    xor ax, ax
+    mov ah, 0 
+    mov al, 12h
+    int 10h
+
+    xor ax, ax
+    xor si, si
+    mov si, message2
+    jmp .print
+
+    .print:
+        lodsb
+        cmp al, 0
+        je .waitButton
+        call putchar
+        jmp .print
+
+    .waitButton:
+        xor ax, ax
+
+        call getchar_m
+
+        cmp al, " "
+        je menu
+
+        jmp waitButton
+
+
 
 endl:
     mov ah, 0x0a
@@ -47,15 +89,13 @@ endl:
     mov ah, 0x0d
     call putchar
     ret
-    
+
 putchar:
     mov ah, 0x0e
     int 10h
     ret
-    
-done:
-    jmp $
 
-times 510 - ($ - $$) db 0
-dw 0xaa55
-
+getchar_m:
+    mov ah, 0x00 
+    int 16h
+    ret
